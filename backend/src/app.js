e2d45e2f73fe31ff6715e89d223bd0b3f00c08f2
Pritish <bordoloipritish97@gmail.com>
2025-02-28 22:08:34 +0530

    connecting to mongo atlas database

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//allows the backend to specify which origins (domains) are allowed to access its resources.
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes
+
+
+
+export { app };
