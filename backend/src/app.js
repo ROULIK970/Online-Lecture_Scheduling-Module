50da9478be7b925320315f29a25951dde58380a6
Pritish <bordoloipritish97@gmail.com>
2025-08-11 12:24:34 +0530

    cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,8 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-//allows the backend to specify which origins (domains) are allowed to access its resources.
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
